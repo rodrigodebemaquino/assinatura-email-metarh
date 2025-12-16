1c0b8745aca326cc3476cefdf6e59ab26e30a920
leandromarques-svg <leandromarques@metarh.com.br>
2025-12-16 18:24:20 -0300

    Recomendações

--- components/SignaturePreview.tsx
+++ components/SignaturePreview.tsx
@@ -129,7 +129,7 @@ const SignaturePreview = forwardRef<HTMLDivElement, SignaturePreviewProps>(({
         </div>
 
         {/* QR Code WhatsApp - Posicionado à direita do texto, na área branca */}
-        <div className="absolute p-1 bg-white border-2 border-[#401669] rounded-lg" style={{ left: '360px', top: '50%', transform: 'translateY(-50%)', zIndex: 10 }}>
+        <div className="absolute p-1 bg-white border-2 border-[#401669] rounded-lg" style={{ left: '360px', top: '71%', transform: 'translateY(-50%)', zIndex: 10 }}>
           <QRCodeSVG
             value={`https://wa.me/55${data.phone.replace(/\D/g, '')}`}
             size={65}
