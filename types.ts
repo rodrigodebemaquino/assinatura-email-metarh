f9a05707b39aaa664d7714a1dc43ee6c5a9dfa27
rodrigodebemaquino <rodrigoaquinodg@gmail.com>
2025-12-16 13:32:22 -0300

    assinatura de email

---
+++ types.ts
@@ -0,0 +1,25 @@
+export interface UserData {
+  name: string;
+  role: string;
+  phone: string;
+  email: string;
+  photoUrl: string | null;
+  photoScale: number;
+  photoX: number;
+  photoY: number;
+}
+
+export interface AdminData {
+  bannerUrl: string | null;
+}
+
+export const DEFAULT_USER_DATA: UserData = {
+  name: 'Rodrigo Aquino',
+  role: 'Marketing',
+  phone: '11 99648-6816',
+  email: 'rodrigoaquino@metarh.com.br',
+  photoUrl: null,
+  photoScale: 1,
+  photoX: 0,
+  photoY: 0,
+};
